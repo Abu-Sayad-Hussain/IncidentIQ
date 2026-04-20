8a4fc44fe289206c3120d3ad81204e8bb1d24c39
abusayadhussain <abusayadhussain@gmail.com>
2026-04-20 23:20:34 +0600

    Fixed typescript issue solve for log service for webconfig

--- apps/log-processor-service/webpack.config.js
+++ apps/log-processor-service/webpack.config.js
@@ -1,18 +1,1 @@
-const { composePlugins, withNx } = require('@nx/webpack');
-const { join } = require('path');
-
-module.exports = composePlugins(
-  withNx({
-    target: 'node',
-    compiler: 'tsc',
-    main: './apps/log-processor-service/src/main.ts',
-    tsConfig: './apps/log-processor-service/tsconfig.app.json',
-    assets: ['./apps/log-processor-service/src/assets'],
-    optimization: false,
-    outputHashing: 'none',
-  }),
-  (config) => {
-    config.output.path = join(__dirname, '../../dist/apps/log-processor-service');
-    return config;
-  }
-);
+const { composePlugins, withNx } = require('@nx/webpack'); module.exports = composePlugins(withNx(), (config) => { return config; });
