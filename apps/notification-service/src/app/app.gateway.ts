73b8095d65050b2179f3a51beddb45b9ec9c4a1b
abusayadhussain <abusayadhussain@gmail.com>
2026-04-23 21:51:14 +0600

    Added gateway  for notification

---
+++ apps/notification-service/src/app/app.gateway.ts
@@ -0,0 +1,23 @@
+import { WebSocketGateway, WebSocketServer, OnGatewayConnection, OnGatewayDisconnect } from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+import { Logger } from '@nestjs/common';
+
+@WebSocketGateway({ cors: { origin: '*' } })
+export class AppGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer()
+  server: Server;
+
+  private logger: Logger = new Logger('AppGateway');
+
+  handleConnection(client: Socket) {
+    this.logger.log(`Client Connected: ${client.id}`);
+  }
+
+  handleDisconnect(client: Socket) {
+    this.logger.log(`Client Disconnected: ${client.id}`);
+  }
+
+  broadcastIncident(incidentData: any) {
+    this.server.emit('incident.new', incidentData);
+  }
+}
