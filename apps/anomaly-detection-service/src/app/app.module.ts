b1784f0fbd24e2bfe3c44f542549d4cf55376631
abusayadhussain <abusayadhussain@gmail.com>
2026-04-27 22:08:18 +0600

    Inject Kafka to the anamoly detection service

--- apps/anomaly-detection-service/src/app/app.module.ts
+++ apps/anomaly-detection-service/src/app/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'KAFKA_SERVICE',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: 'anomaly-detection',
+            brokers: [process.env.KAFKA_BROKER || 'localhost:9092'],
+          },
+          producerOnlyMode: true,
+        },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
