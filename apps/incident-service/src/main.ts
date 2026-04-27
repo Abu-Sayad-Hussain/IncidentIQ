b8cc0a880ae68fa9a23997354b40b6c0856bfdbe
abusayadhussain <abusayadhussain@gmail.com>
2026-04-27 22:09:51 +0600

    Added connect micro service to the incident service

--- apps/incident-service/src/main.ts
+++ apps/incident-service/src/main.ts
@@ -1,21 +1,33 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { Partitioners } from 'kafkajs';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // Act as a Kafka Consumer
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: [process.env.KAFKA_BROKER || 'localhost:9092'],
+      },
+      consumer: {
+        groupId: 'incident-service-consumer',
+      },
+      producer: {
+        createPartitioner: Partitioners.LegacyPartitioner,
+      }
+    },
+  });
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 3004; // Distinct port
+  
+  await app.startAllMicroservices();
   await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
-  );
+  console.log(`Incident Service HTTP listening on http://localhost:${port}/${globalPrefix}`);
 }
-
 bootstrap();
