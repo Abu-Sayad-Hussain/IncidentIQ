a7ea75067cadfa4b965d4dded59a0f6affa54fe9
abusayadhussain <abusayadhussain@gmail.com>
2026-04-27 22:12:39 +0600

    Added connect microservice with kafka for notification service

--- apps/notification-service/src/main.ts
+++ apps/notification-service/src/main.ts
@@ -1,4 +1,6 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { Partitioners } from 'kafkajs';
 import { AppModule } from './app/app.module';
 import { RedisIoAdapter } from './redis-io.adapter';
 
@@ -8,10 +10,26 @@ async function bootstrap() {
 
   const redisIoAdapter = new RedisIoAdapter(app);
   await redisIoAdapter.connectToRedis();
-  
   app.useWebSocketAdapter(redisIoAdapter);
 
+  // Bind Kafka to implicitly receive 'incident.new'
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: [process.env.KAFKA_BROKER || 'localhost:9092'],
+      },
+      consumer: {
+        groupId: 'notification-service-consumer',
+      },
+      producer: {
+        createPartitioner: Partitioners.LegacyPartitioner,
+      }
+    },
+  });
+
   const port = process.env.PORT || 3003;
+  await app.startAllMicroservices();
   await app.listen(port);
   console.log(`Notification Service (WebSockets) listening on ws://localhost:${port}`);
 }
